663f4d23754f95a31f6c02249bed66b0433f6316
adgadgad <137925334+adgadgad@users.noreply.github.com>
2024-06-27 14:21:50 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,1 @@
-from setuptools import setup, find_packages
 
-setup(
-    name='yourpackage',
-    version='1.0.0',
-    description='Your project description',
-    packages=find_packages(),
-    install_requires=[
-        'numpy',
-        'pandas',
-        'requests',
-        'ta',
-        'alpha_vantage',
-        'scikit-learn'
-    ]
-)
